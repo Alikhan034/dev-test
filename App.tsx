1874c64f752334dbf2d16616a9698f2c133cbb22
Muhammad Ali Khan <alikhan809212@gmail.com>
2024-01-28 00:14:39 +0500

    add ali khan

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>hello</Text>
+      <Text>Ali Khan</Text>
       <StatusBar style="auto" />
     </View>
   );
