8f6750b5838077e8a89777b74d299d86c3aea4d2
Muhammad Ali Khan <alikhan809212@gmail.com>
2024-01-28 18:32:09 +0500

    making of calender

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View ,FlatList} from 'react-native';
+const days=[1,2,3,4,5,6,7];
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>hello</Text>
-      <Text>Ali Khan</Text>
+      <FlatList
+      data={days}
+      contentContainerStyle={styles.content}
+      columnWrapperStyle={styles.column}
+      numColumns={2}
+      renderItem={({item})=>(
+        <View style={styles.box}>
+        <Text style={styles.txt}>{item}</Text>
+      </View>
+
+      )}
+   
+      />
+
+      
+
+
+      
+
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,7 +36,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
+    
   },
+  content:{
+    
+
+  },
+  box:{
+    backgroundColor:'#f9EDE3',
+    /* width:100,
+    height:100, */
+    flex:1,
+    aspectRatio:1,
+  
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth:StyleSheet.hairlineWidth,
+    borderColor: '#9b4521',
+    borderRadius:20,
+    
+  },
+  column:{
+    gap:10,
+    padding:10,
+  
+  },
+  txt:{
+    color:'#9b4521',
+    fontSize: 50,
+  }
 });
