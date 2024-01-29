bd00f9a4c1f7f875e5215f00b3bde980f1219bff
Muhammad Ali Khan <alikhan809212@gmail.com>
2024-01-29 15:45:12 +0500

    just nothing

--- App.tsx
+++ App.tsx
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View ,FlatList} from 'react-native';
-const days=[1,2,3,4,5,6,7];
+import DayListItem from './src/components/core/DayListItem';
+
+
+const days=[...Array(24)].map((val,index)=>index+1);
 export default function App() {
 
   return (
     <View style={styles.container}>
+    
       <FlatList
       data={days}
       contentContainerStyle={styles.content}
       columnWrapperStyle={styles.column}
       numColumns={2}
-      renderItem={({item})=>(
-        <View style={styles.box}>
-        <Text style={styles.txt}>{item}</Text>
-      </View>
-
-      )}
+      renderItem={({item})=>(<DayListItem day={item} />  )}
    
       />
 
@@ -43,27 +42,12 @@ const styles = StyleSheet.create({
     
 
   },
-  box:{
-    backgroundColor:'#f9EDE3',
-    /* width:100,
-    height:100, */
-    flex:1,
-    aspectRatio:1,
   
-    justifyContent:'center',
-    alignItems:'center',
-    borderWidth:StyleSheet.hairlineWidth,
-    borderColor: '#9b4521',
-    borderRadius:20,
-    
-  },
+
   column:{
     gap:10,
     padding:10,
   
   },
-  txt:{
-    color:'#9b4521',
-    fontSize: 50,
-  }
+ 
 });
---
+++ src/components/core/DayListItem.tsx
@@ -0,0 +1,38 @@
+import { Text,View,StyleSheet } from "react-native";
+type DayListItem={
+    day:number;
+}
+
+export default function DayListItem({day}){
+    return(
+
+        <View style={styles.box}>
+        <Text style={styles.txt}>{day}</Text>
+      </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+   
+    box:{
+      backgroundColor:'#f9EDE3',
+      /* width:100,
+      height:100, */
+      flex:1,
+      aspectRatio:1,
+    
+      justifyContent:'center',
+      alignItems:'center',
+      borderWidth:StyleSheet.hairlineWidth,
+      borderColor: '#9b4521',
+      borderRadius:20,
+      
+    },
+    
+    txt:{
+      color:'#9b4521',
+      fontSize: 50,
+    }
+  });
+  
